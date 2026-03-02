d2a3ed031be6022101de02e1fb9b8a8261cbd5c8
Akshadha <ar4847@srmist.edu.in>
2026-03-02 14:55:07 +0530

    UC1: Add welcome message module

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,2 +1,37 @@
+
+import java.util.Scanner;
+
 public class PalindromeCheckerApp {
+
+
+    public static boolean isPalindrome(String input) {
+        String cleaned = input.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
+
+        int left = 0;
+        int right = cleaned.length() - 1;
+
+        while (left < right) {
+            if (cleaned.charAt(left) != cleaned.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter a word or sentence:");
+        String input = scanner.nextLine();
+
+        if (isPalindrome(input)) {
+            System.out.println("It is a palindrome ✅");
+        } else {
+            System.out.println("It is NOT a palindrome ❌");
+        }
+
+        scanner.close();
+    }
 }
